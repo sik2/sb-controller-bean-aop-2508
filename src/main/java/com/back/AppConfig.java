5370ec5dd8b4e9e865f23c58a6e260e19150f00d
sik2 <bickwang123@gmail.com>
2025-08-21 14:43:26 +0900

    feat

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +15,31 @@ public class AppConfig {
     @Bean
     int version() {
         return 3;
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner() {
+        return new ApplicationRunner() {
+            @Override
+            public void run (ApplicationArguments args) throws Exception {
+                System.out.println("myApplicationRunner is running");
+            }
+        };
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner2() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1");
+    }
+
+    private void work2() {
+        System.out.println("work2");
     }
 }
