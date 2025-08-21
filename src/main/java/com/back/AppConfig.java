edb8f63a68cb889fe1d24f1f86c1c0815f739ac6
sik2 <bickwang123@gmail.com>
2025-08-21 14:32:52 +0900

    myApplicationRunner2

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -3,6 +3,7 @@ package com.back;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
@@ -17,12 +18,14 @@ public class AppConfig {
     }
 
     @Bean
+    @Order(2)
     public ApplicationRunner myApplicationRunner() {
         return new MyApplicationRunner();
     }
 
     @Bean
+    @Order(1)
     public ApplicationRunner myApplicationRunner2() {
-        return new MyApplicationRunner();
+        return new MyApplicationRunner2();
     }
 }
