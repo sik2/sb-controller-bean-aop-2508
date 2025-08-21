637ece022da615c0a985d7aa2009398cccdf59b7
sik2 <bickwang123@gmail.com>
2025-08-21 11:46:37 +0900

    init

---
+++ src/main/java/com/back/ControllerBeanAop2508Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerBeanAop2508Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerBeanAop2508Application.class, args);
+    }
+
+}
