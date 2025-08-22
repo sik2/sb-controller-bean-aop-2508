7c74ad170e114ea0f739b20abc86103bd2f988db
sik2 <bickwang123@gmail.com>
2025-08-22 14:41:06 +0900

    rollback

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +9,9 @@ import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class AppConfig {
-    private final AppConfig self;
-
-    public AppConfig(@Lazy AppConfig appConfig) {
-        this.self = appConfig;
-    }
+    @Autowired
+    @Lazy
+    private AppConfig self;
 
     @Bean
     PersonRepository personRepository() {
