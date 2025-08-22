9969a5364c9651631058ee425c7ca0b429db3b0c
sik2 <bickwang123@gmail.com>
2025-08-22 14:29:15 +0900

    rollback

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -2,16 +2,12 @@ package com.back;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
-@Validated
 public class PersonService {
     private final PersonRepository personRepository;
 
-    @Transactional
     public long count() {
         return personRepository.count();
     }
